409dcbb8f3c912cd45a2b33ee3e75979fa80fc90
kemboi22 <kemboielvis22@gmail.xom>
2023-01-12 14:16:56 +0300

    Stk Push complete With chaining methods available

--- src/Helpers/Stk.php
+++ src/Helpers/Stk.php
@@ -6,23 +6,30 @@ use Kemboielvis\MpesaSdkPhp\Mpesa;
 
 class Stk extends Mpesa
 {
-    public function __construct(array $data)
+    public function __construct($data)
     {
+        parent::__construct([
+            "consumer_secret" =>$this->consumer_secret,
+            "consumer_key" => $this->consumer_key
+        ]);
+
         $this->configure($data);
     }
 
     public function configure(array $data)
     {
+
         if (array_key_exists("business_code", $data)){
             if (array_key_exists("transaction_type", $data)){
                 if (array_key_exists("amount", $data)){
                     if (array_key_exists("phone_number", $data)){
                         if (array_key_exists("call_back_url", $data)){
                             $this->business_code = $data["business_code"];
-                            $this->type_of_transaction = $data["transaction_type"];
+                            $this->transaction_type = $data["transaction_type"];
                             $this->amount = $data["amount"];
                             $this->phone_number = (int)$data["phone_number"];
                             $this->call_back_url = $data["call_back_url"];
+                            return $this;
 
                         }else{
                             throw new \Exception("Call Back url is required");
@@ -40,6 +47,7 @@ class Stk extends Mpesa
             throw new \Exception("A Business Code is required");
         }
 
+
     }
 
     public function push(){
@@ -48,7 +56,7 @@ class Stk extends Mpesa
             "BusinessShortCode" => $this->business_code,
             "Password" => $this->password(),
             "Timestamp" => $this->timestamp(),
-            "TransactionType" => $this->type_of_transaction,
+            "TransactionType" => $this->transaction_type,
             "Amount" => $this->amount,
             "PartyA" => $this->phone_number,
             "PartyB" => $this->business_code,
@@ -60,5 +68,41 @@ class Stk extends Mpesa
         ];
         return $this->curls($array_data, $this->stk_push_url);
     }
+    public function business_code(string $business_code): static
+    {
+        $this->business_code = $business_code;
+        return $this;
+    }
+
+    public function pass_key(string $pass_key): static
+    {
+        $this->pass_key = $pass_key;
+        return $this;
+    }
+    public function transaction_type(string $transaction_type): static
+    {
+        $this->transaction_type = $transaction_type;
+        return $this;
+    }
+    public function phone_number(string $phone): static
+    {
+//        if($phone[0] == "+") $phone = substr($phone, 1);
+//        if($phone[0] == "0") $phone = substr($phone, 1);
+//        if($phone[0] == "7") $phone = "254" . $phone;
+
+        $this->phone_number = $phone;
+        return $this;
+    }
+    public  function amount(int $amount): static
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+    public function call_back_url($url): static
+    {
+        $this->call_back_url = $url;
+        return $this;
+    }
+
 
 }
--- src/Mpesa.php
+++ src/Mpesa.php
@@ -6,24 +6,46 @@ use Kemboielvis\MpesaSdkPhp\Helpers\Stk;
 
 class Mpesa
 {
-    public string $consumer_key = "Kuu9J6dcwaSnZjr3iyHKIwXvFCXkkt8y";
-    public string $consumer_secret = "ljjSvwG5uvvTccdG";
+    public string $consumer_key = "";
+    public string $consumer_secret = "";
     public  string $business_code = "";
     public string $pass_key = "";
-    public string $type_of_transaction = "";
+    public string $transaction_type = "";
     public string $token_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
     public string $phone_number = "";
     public string $onine_payment = "";
     public string $amount = "";
     public string $call_back_url = "https://mydomain.com/path";
     public string $stk_push_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+    public string $token = "";
 
-    public string $partyB = "";
+    public function __construct($data)
+    {
+        $this->configure($data);
+
+    }
+
+    public function configure(array $data){
+        if (array_key_exists("consumer_key", $data)){
+            if (array_key_exists("consumer_secret", $data)){
+               $this->consumer_key = (string)$data["consumer_key"];
+               $this->consumer_secret = (string)$data["consumer_secret"];
+               return $this;
+            }else{
+                throw new \Exception("Consumer Key is required");
+            }
+        }else{
+            throw new \Exception("Consumer Secret is required");
+        }
+    }
+
+
     public function timestamp(): string
     {
-        return date("Ymdhis");
+        return date("YmdHis");
     }
-    public function password(){
+    public function password(): string
+    {
         return base64_encode($this->business_code.$this->pass_key.$this->timestamp());
     }
 
@@ -31,7 +53,6 @@ class Mpesa
         $curl_transfer = curl_init();
         curl_setopt($curl_transfer, CURLOPT_URL, $this->token_url);
         $credentials = base64_encode($this->consumer_key.":".$this->consumer_secret);
-
         curl_setopt($curl_transfer, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials));
         curl_setopt($curl_transfer, CURLOPT_HEADER, false);
         curl_setopt($curl_transfer, CURLOPT_RETURNTRANSFER, 1);
@@ -43,23 +64,44 @@ class Mpesa
     }
 
     public function curls(array $data, $url){
-        $curl_transfer = curl_init();
-        curl_setopt($curl_transfer, CURLOPT_URL, $url);
-        curl_setopt($curl_transfer, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization:Bearer '.$this->authentication_token()));
-        $json_string = json_encode($data);
-        curl_setopt($curl_transfer, CURLOPT_POST, 1);
-        curl_setopt($curl_transfer, CURLOPT_POSTFIELDS, $json_string);
-        curl_setopt($curl_transfer, CURLOPT_RETURNTRANSFER, 1);
+        $curl_transfer = curl_init($url);
+        curl_setopt($curl_transfer, CURLOPT_HTTPHEADER, ["Content-Type: application/json", "Authorization: Bearer ".$this->authentication_token()]);
+
+        curl_setopt($curl_transfer, CURLOPT_POST, true);
+        curl_setopt($curl_transfer, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($curl_transfer, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl_transfer, CURLOPT_HEADER, false);
-        curl_setopt($curl_transfer, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($curl_transfer, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($curl_transfer);
         curl_close($curl_transfer);
-        return $response;
+        return json_decode($response);
     }
 
-    public function stk($data){
-        return new Stk($data);
+    public function stk(){
+        return new Stk([
+            "business_code" => $this->business_code,
+            "transaction_type" => $this->transaction_type,
+            "amount" => $this->amount,
+            "phone_number" => $this->phone_number,
+            "call_back_url" => "https://mydomain.com/path"
+        ]);
     }
+
+    public function consumer_key(string $consumer_key){
+        $this->consumer_key = $consumer_key;
+        return $this;
+    }
+
+    public function consumer_secret(string $consumer_secret){
+        $this->consumer_secret = $consumer_secret;
+        return $this;
+    }
+
+    public function pass_key(string $pass_key){
+        $this->pass_key = $pass_key;
+        return $this;
+    }
+
+
+
 
 }
