bf0167b094ee431aedf2c28e84c1b18ba07ecc51
kemboi22 <kemboielvis22@gmail.com>
2025-03-18 04:10:58 +0300

    feat: added return types and added param types

--- src/Helpers/Stk.php
+++ src/Helpers/Stk.php
@@ -10,106 +10,111 @@ class Stk extends Mpesa
     {
         $this->configure($data);
     }
-    private string $account_reference = "";
+    private string $account_reference = '';
 
-    private string  $transaction_desc = "";
-    public function configure(array $data)
+    private string $transaction_desc = '';
+
+    private $baseUrl;
+
+    /**
+     * @param array<int,mixed> $data
+     */
+    public function configure(array $data): Stk
     {
+        if (array_key_exists('business_code', $data)) {
+            if (array_key_exists('transaction_type', $data)) {
+                if (array_key_exists('amount', $data)) {
+                    if (array_key_exists('phone_number', $data)) {
+                        if (array_key_exists('call_back_url', $data)) {
+                            $this->consumer_key = $data['consumer_key'];
+                            $this->consumer_secret = $data['consumer_secret'];
+                            $this->business_code = $data['business_code'];
+                            $this->transaction_type = $data['transaction_type'];
+                            $this->amount = $data['amount'];
+                            $this->phone_number = (int) $data['phone_number'];
+                            $this->call_back_url = $data['call_back_url'];
+                            $this->baseUrl = $data['base_url'];
 
-        if (array_key_exists("business_code", $data)){
-            if (array_key_exists("transaction_type", $data)){
-                if (array_key_exists("amount", $data)){
-                    if (array_key_exists("phone_number", $data)){
-                        if (array_key_exists("call_back_url", $data)){
-                            $this->consumer_key = $data["consumer_key"];
-                            $this->consumer_secret = $data["consumer_secret"];
-                            $this->business_code = $data["business_code"];
-                            $this->transaction_type = $data["transaction_type"];
-                            $this->amount = $data["amount"];
-                            $this->phone_number = (int)$data["phone_number"];
-                            $this->call_back_url = $data["call_back_url"];
-                            $this->baseUrl = $data["base_url"];
                             return $this;
-
-                        }else{
-                            throw new \Exception("Call Back url is required");
                         }
-                    }else{
-                        throw new \Exception("A phone Number is required");
+                        throw new \Exception('Call Back url is required');
+                    } else {
+                        throw new \Exception('A phone Number is required');
                     }
-                }else{
-                    throw new \Exception("An Amount is required");
+                } else {
+                    throw new \Exception('An Amount is required');
                 }
-            }else{
-                throw new \Exception("A transaction type is required");
+            } else {
+                throw new \Exception('A transaction type is required');
             }
-        }else{
-            throw new \Exception("A Business Code is required");
+        } else {
+            throw new \Exception('A Business Code is required');
         }
-
-
     }
 
-    public function push(){
-
+    public function push(): Stk
+    {
         $array_data = [
-            "BusinessShortCode" => $this->business_code,
-            "Password" => $this->password(),
-            "Timestamp" => $this->timestamp(),
-            "TransactionType" => $this->transaction_type,
-            "Amount" => $this->amount,
-            "PartyA" => $this->phone_number,
-            "PartyB" => $this->business_code,
-            "PhoneNumber" => $this->phone_number,
-            "CallBackURL" => $this->call_back_url,
-            "AccountReference" => $this->account_reference,
-            "TransactionDesc" => $this->transaction_desc
+            'BusinessShortCode' => $this->business_code,
+            'Password' => $this->password(),
+            'Timestamp' => $this->timestamp(),
+            'TransactionType' => $this->transaction_type,
+            'Amount' => $this->amount,
+            'PartyA' => $this->phone_number,
+            'PartyB' => $this->business_code,
+            'PhoneNumber' => $this->phone_number,
+            'CallBackURL' => $this->call_back_url,
+            'AccountReference' => $this->account_reference,
+            'TransactionDesc' => $this->transaction_desc,
 
         ];
-        $this->response = $this->curls($array_data, "/mpesa/stkpush/v1/processrequest");
+        $this->response = $this->curls($array_data, '/mpesa/stkpush/v1/processrequest');
+
         return $this;
     }
 
-
-
-    public function transactionType(string $transaction_type): static
+    public function transactionType(string $transaction_type): Stk
     {
         $this->transaction_type = $transaction_type;
+
         return $this;
     }
 
-    public function  accountReference($account_reference): static
+    public function accountReference(string $account_reference): Stk
     {
         $this->account_reference = $account_reference;
+
         return $this;
     }
 
-    public function transactionDesc($transaction_desc):static
+    public function transactionDesc(string $transaction_desc): Stk
     {
         $this->transaction_desc = $transaction_desc;
+
         return $this;
     }
 
-
-    public function callBackUrl($url): static
+    public function callBackUrl(string $url): Stk
     {
         $this->call_back_url = $url;
+
         return $this;
     }
-    public function checkoutId(){
+
+    public function checkoutId()
+    {
         return $this->response->CheckoutRequestID;
     }
-
 
     public function query()
     {
         $array_data = [
-            "BusinessShortCode" => $this->business_code,
-            "Password" => $this->password(),
-            "Timestamp" => $this->timestamp(),
-            "CheckoutRequestID" => $this->checkoutId()
+            'BusinessShortCode' => $this->business_code,
+            'Password' => $this->password(),
+            'Timestamp' => $this->timestamp(),
+            'CheckoutRequestID' => $this->checkoutId(),
         ];
-        return $this->curls($array_data, "/mpesa/stkpushquery/v1/query");
-    }
 
+        return $this->curls($array_data, '/mpesa/stkpushquery/v1/query');
+    }
 }
